9d380fcd0d8e846ade13f4927ba4947a9e325387
Andreas **Felix** Häberle <felix@42sol.eu>
2025-06-07 16:54:37 +0200

    Add: Lenses and charger.

--- src/model123d/belta/cmf_phone_pro_2.py
+++ src/model123d/belta/cmf_phone_pro_2.py
@@ -13,7 +13,7 @@ project:
 # [Imports]
 from build123d import *
 from build123d import MM as mm
-from ocp_vscode import show
+from ocp_vscode import show, set_defaults, Camera
 from dataclasses import dataclass
 from pathlib import Path
 import sys
@@ -55,11 +55,12 @@ def define(object, color="#ff0000", name=""):
 @dataclass
 class Parameters:
     """Parameters for the ${TM_FILENAME}"""
-    show_debug: bool         = yes
+    show_debug: bool         = no
     show_selection: bool     = no
     show_nose: bool          = no
     do_details: bool         = no
     do_screws: bool          = yes
+    do_charger: bool         = yes
     do_post_processing: bool = no
     do_export: bool          = yes
     
@@ -68,6 +69,7 @@ class Parameters:
     body_radius: float            =   9. * mm
     body_width: float             =  78.0 * mm
     body_extrude: float           =   7.8 - 1.5 + 0.2 * mm
+    display_extrude: float        =   1.0 * mm
     backplate_extrude: float      =   1.3 * mm
     hole_diameter: float          =   2.0 * mm
 
@@ -87,33 +89,34 @@ class Parameters:
     ext_position_y: float         =  10. * mm
     ext_position_x: float         =  78. - 10. * mm
     ext_diameter: float           =  2.5 * mm
+    ext_radius: float             = 19.52/2 * mm
     
     # Charger dimensions
     charger_position_x: float     =  78. / 2. * mm
     charger_position_y: float     =  75. * mm
-    charger_inner_diameter: float =  50. * mm
+    charger_inner_diameter: float =  46. * mm
     charger_outer_diameter: float =  54. * mm
     
     # Camera dimensions
     camera1_x: float         =  3.9 + 23.96 * mm
-    camera1_y: float         =  142.5 * mm
+    camera1_y: float         =  143.0 * mm
     camera1_w: float         =  10.6 * mm
     camera1_h: float         =  22.0 * mm
     camera1_r: float         =   5.0 * mm
 
     camera2_x: float         =  11. * mm
-    camera2_y: float         =  153.5 * mm
+    camera2_y: float         =  152.5 * mm
     camera2_diameter: float  =  19. * mm
 
     camera3_x: float         =  11. * mm
     camera3_y: float         =  153.5 - 22. * mm
     camera3_diameter: float  =  19. * mm
 
-    camera4_x: float         =  11. * mm
-    camera4_y: float         =  142.5 * mm
-    camera4_w: float         =  20. * mm
-    camera4_h: float         =  22. + 19. * mm
-    camera4_r: float         =   9.5 * mm
+    cameraF_x: float         =  11. * mm
+    cameraF_y: float         =  142.5 * mm
+    cameraF_w: float         =  20. * mm
+    cameraF_h: float         =  22. + 19. * mm
+    cameraF_r: float         =   9.5 * mm
     
     # Plugs dimensions
     usbc_width: float         =   8.4 * mm
@@ -125,10 +128,72 @@ class Parameters:
 
 
 P = Parameters()
+set_defaults(reset_camera=Camera.KEEP,)
 
 
-# [Calculate parameters]
+# [Helper models]
 
+def do_camera(x, y, z, diameter, frame, height, name="camera"):
+    """Create a camera with a frame"""
+    outer_radius = diameter / 2
+    inner_radius = (diameter - frame) / 2
+    
+    with BuildPart(Plane.XY.offset(z)) as bottom:
+        with Locations((x, y)):
+            Cylinder(inner_radius, 0.01, mode=Mode.ADD)
+
+    with BuildPart(Plane.XY.offset(z+0.01)) as outer:
+        with Locations((x, y)):
+            Cylinder(inner_radius*0.4, 0.01, mode=Mode.ADD)
+
+    with BuildPart(Plane.XY.offset(z+0.02)) as inner:
+        with Locations((x, y)):
+            Cylinder(inner_radius*0.3, 0.01, mode=Mode.ADD)
+    
+    with BuildPart(Plane.XY.offset(z)) as frame:
+        with BuildSketch(Plane.XY.offset(z)) as frame_sketch:
+            with Locations((x, y)):
+                Circle(outer_radius, mode=Mode.ADD)
+                Circle(inner_radius, mode=Mode.SUBTRACT)
+        extrude(amount=height, mode=Mode.ADD)
+    
+    with BuildPart(Plane.XY.offset(z+height/2)) as lense:
+        with Locations((x, y)):
+            Cylinder(inner_radius, height-0.05, mode=Mode.ADD)
+        
+    
+    define (bottom, "#000000ff", f'{name}_bottom')
+    define (outer, "#444343bb", f'{name}_outer')
+    define (inner, "#282877ff", f'{name}_inner')
+    define (frame,  "#bbbdbbff", f'{name}_frame')
+    define (lense,  "#f4f44b2c", f'{name}_lens')
+
+def do_led(x, y, z, diameter, name="led"):
+    with BuildPart(Plane.XY.offset(z+0.01)) as outer:
+        with Locations((x, y)):
+            Cylinder(diameter, 0.01, mode=Mode.ADD)
+
+    with BuildPart(Plane.XY.offset(z+0.02)) as inner:
+        with Locations((x, y)):
+            Cylinder(diameter*0.5, 0.01, mode=Mode.ADD)
+
+    define (outer,  "#bbbdbbff", f'{name}_outer')
+    define (inner,  "#f4f44b2c", f'{name}_inner')
+
+def do_screw(x, y, z, diameter, name="screw"):
+    """Create a screw with a head"""
+    head_diameter = diameter * 2.5
+    head_height = diameter * 0.5
+    # TODO: implement and add this 
+    with BuildPart(Plane.XY.offset(z)) as screw:
+        with Locations((x, y)):
+            Cylinder(diameter/2, head_height, mode=Mode.ADD)
+    
+    with BuildPart(Plane.XY.offset(z+head_height)) as head:
+        with Locations((x, y)):
+            Cylinder(head_diameter/2, 0.01, mode=Mode.ADD)
+    
+    define (screw, "#ccccd3ff", f'{name}_screw')
 
 # [Main]    
 if __name__ == "__main__":    
@@ -142,13 +207,31 @@ if __name__ == "__main__":
         backplate = import_stl(stl_path)
         backplate.move(loc=Location((-P.body_width-8,-23.35,P.body_extrude-0.8)))
         # .rotate(Axis.Z, 45)  # Rotate the backplate to match the orientation
-        define(backplate, "#ffffffdd", "backplate")
-        debug(f"Imported STL part from {stl_path}. {type(backplate)}")
+        if yes or P.show_debug:
+            define(backplate, "#ffffffdd", "backplate_imported")
+            debug(f"Imported STL part from {stl_path}. {type(backplate)}")
     except Exception as e:
         debug(f"Failed to import STL: {e}")
     
     
     debug("Creating model") 
+
+    with BuildPart() as display_cutout:
+        with BuildSketch(Plane.XY) as bottom_sketch:
+            RectangleRounded(P.body_width, P.body_height, P.body_radius,
+                            align=(Align.MIN, Align.MIN))
+        extrude(amount=P.display_extrude, mode=Mode.ADD)        
+    
+    with BuildPart() as display:
+        with BuildSketch(Plane.XY) as bottom_sketch:
+            RectangleRounded(P.body_width, P.body_height, P.body_radius,
+                            align=(Align.MIN, Align.MIN))
+        extrude(amount=P.display_extrude, mode=Mode.ADD)        
+        bottom_face = display.faces().filter_by(Plane.XY)[0]
+        edge = bottom_face.edges()[-1]
+        fillet(edge, P.display_extrude/2)
+        
+            
     with BuildPart() as phone:
         with BuildSketch(Plane.XY) as bottom_sketch:
             RectangleRounded(P.body_width, P.body_height, P.body_radius,
@@ -166,57 +249,28 @@ if __name__ == "__main__":
                 with Locations(
                     (P.ext_position_x, P.ext_position_y)):
                     Circle(P.ext_diameter/2, mode=Mode.SUBTRACT)
-
-
-            if P.do_details:
-                with Locations(
-                    (P.charger_position_x, P.charger_position_y)):
-                    Circle(P.charger_outer_diameter/2, mode=Mode.SUBTRACT)
-                    Circle(P.charger_inner_diameter/2, mode=Mode.ADD)
-            
-                with Locations(
-                    (P.camera1_x, P.camera1_y)):
-                    RectangleRounded(P.camera1_w, P.camera1_h, P.camera1_r, mode=Mode.SUBTRACT)
-                with Locations(
-                    (P.camera1_x, P.camera1_y+P.camera1_h/4)):
-                    Circle(P.camera1_r-0.5, mode=Mode.ADD)
-                with Locations(
-                    (P.camera1_x, P.camera1_y-P.camera1_h/4)):
-                    Circle(P.camera1_r/2, mode=Mode.ADD)
-
-                with Locations(
-                    (P.camera4_x, P.camera4_y)):
-                    RectangleRounded(P.camera4_w, P.camera4_h, P.camera4_r, mode=Mode.SUBTRACT)
-                    
-                with Locations(
-                    (P.camera2_x, P.camera2_y)):
-                    Circle(P.camera2_diameter/2, mode=Mode.ADD)
-                    Circle((P.camera2_diameter-5)/2, mode=Mode.SUBTRACT)
-
-                with Locations(
-                    (P.camera3_x, P.camera3_y)):
-                    Circle(P.camera3_diameter/2, mode=Mode.ADD)
-                    Circle((P.camera3_diameter-5)/2, mode=Mode.SUBTRACT)
         
         extrude(amount=P.body_extrude, mode=Mode.ADD)
         
         active = phone.faces().filter_by(Plane.XZ)
         bottom_face = active[0]
-        define(bottom_face, "#ff0000aa", "bottom_face")
         top_face    = active[1]
-        define(top_face, "#00ff00aa", "top_face")
         
         active = phone.faces().filter_by(Plane.YZ).filter_by(lambda f: f)
         left_face  = active[0]
-        define(left_face, "#00ffffff", "left_face")
         right_face = active[1]
-        define(right_face, "#ff00ffff", "right_face")
         
         active = phone.faces().filter_by(Plane.XY).filter_by(lambda f: f)
         display_face  = active[0]
-        define(display_face, "#eeff00ff", "display_face")
         back_face = active[1]
-        define(back_face, "#f90404", "back_face")
+        
+        if P.show_debug:
+            define(bottom_face, "#ff0000aa", "bottom_face")
+            define(top_face, "#00ff00aa", "top_face")
+            define(left_face, "#00ffffff", "left_face")
+            define(right_face, "#ff00ffff", "right_face")
+            define(display_face, "#eeff00ff", "display_face")
+            define(back_face, "#f90404", "back_face")
         
         with BuildSketch(bottom_face) as bottom_sketch:
             # Define the sketch on the bottom face
@@ -225,7 +279,7 @@ if __name__ == "__main__":
                                 P.usbc_inside_height/4, mode=Mode.SUBTRACT)
             
         extrude(amount=-P.usbc_height, mode=Mode.SUBTRACT)
-        
+        add(display_cutout.part, mode=Mode.SUBTRACT)
         edges = phone.edges().filter_by(lambda e: e)
         for index in range(len(edges)):
             if P.show_selection:
@@ -236,8 +290,21 @@ if __name__ == "__main__":
             # chamfer(edges[32],2.)
             # chamfer(edges[36],2.)
 
-    define(phone, "#0000aaff", "phone")
+    define(phone,   "#ccccd3ff", "phone")
+    define(display, "#000000ff", "display")
 
+    P.camera1_frame = 1.0
+    P.camera2_frame = 5.0
+    P.camera3_frame = 5.0
+    P.camera1_extrude = 1.0
+    P.camera2_extrude = 2.0
+    P.camera3_extrude = 2.0
+    
+    do_camera(P.camera1_x, P.camera1_y+P.camera1_h/4, P.body_extrude, P.camera1_r*2-0.1, P.camera1_frame, P.camera1_extrude,      'camera1')
+    do_led(P.camera1_x, P.camera1_y-P.camera1_h/4, P.body_extrude, P.camera1_r*0.6, 'led')
+    do_camera(P.camera2_x, P.camera2_y, P.body_extrude, P.camera3_diameter-0.1, P.camera2_frame, P.camera2_extrude, 'camera2')
+    do_camera(P.camera3_x, P.camera3_y, P.body_extrude, P.camera3_diameter-0.1, P.camera3_frame, P.camera3_extrude, 'camera3')
+                    
 
 
     with BuildPart(Plane.XY.offset(P.body_extrude)) as backplate_screws:
@@ -250,12 +317,10 @@ if __name__ == "__main__":
             with Locations(
                 (P.camera2_x, P.camera2_y)):
                 Circle(P.camera2_diameter/2, mode=Mode.ADD)
-                Circle((P.camera2_diameter-5)/2, mode=Mode.ADD)
 
             with Locations(
                 (P.camera3_x, P.camera3_y)):
                 Circle(P.camera3_diameter/2, mode=Mode.ADD)
-                Circle((P.camera3_diameter-5)/2, mode=Mode.ADD)
             
         with BuildSketch() as screws:
             if P.do_screws:
@@ -271,6 +336,15 @@ if __name__ == "__main__":
                     (P.ext_position_x, P.ext_position_y)):
                     Circle(P.ext_diameter/2, mode=Mode.ADD)
         extrude(amount=P.backplate_extrude, mode=Mode.ADD)
+
+        if P.do_charger:
+            with BuildSketch() as cuts:
+                with Locations(
+                    (P.charger_position_x, P.charger_position_y)):
+                    Circle(P.charger_outer_diameter/2, mode=Mode.ADD)
+                    Circle(P.charger_inner_diameter/2, mode=Mode.SUBTRACT)
+            extrude(amount=P.backplate_extrude*2, mode=Mode.ADD)
+        
 
 
     with BuildPart(Plane.XY.offset(P.body_extrude)) as backplate:
@@ -297,12 +371,28 @@ if __name__ == "__main__":
             with Locations(
                 (P.ext_position_x, P.ext_position_y, P.backplate_extrude),
                 ):
-                Cylinder(19.52/2, P.backplate_extrude, mode=Mode.SUBTRACT)
+                Cylinder(P.ext_radius, P.backplate_extrude, mode=Mode.SUBTRACT)
+
+            with Locations(
+                (P.ext_position_x+P.ext_radius/2, P.ext_position_y-P.ext_radius/2, P.backplate_extrude),
+                ):
+                Box(P.ext_radius*2, P.ext_radius*2, P.backplate_extrude, 
+                    mode=Mode.SUBTRACT)
         
         add(backplate_screws.part, mode=Mode.SUBTRACT)
 
+    with BuildPart(Plane.XY.offset(P.body_extrude)) as charger:
+        if P.do_charger:
+            with BuildSketch(Plane.XY.offset(P.body_extrude)) as cuts:
+                with Locations(
+                    (P.charger_position_x, P.charger_position_y)):
+                    Circle(P.charger_outer_diameter/2, mode=Mode.ADD)
+                    Circle(P.charger_inner_diameter/2, mode=Mode.SUBTRACT)
+            extrude(amount=P.backplate_extrude*2, mode=Mode.ADD)
+
         
     define(backplate, "#e19e18ff", "backplate_2")
+    define(charger, "#7c5b18ff", "charger")
     # Show the box
     show(*objects)
     
@@ -317,6 +407,7 @@ if __name__ == "__main__":
         export_path = Path(__file__).parent / export_name
         console.log(f"[green]Model exported to {export_path}[/green]")
         exporter = Mesher()
+        exporter.add_shape(display.part)
         exporter.add_shape(phone.part)
         exporter.add_shape(backplate.part)
         exporter.write(export_path)
